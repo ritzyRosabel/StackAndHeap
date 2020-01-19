bc05e4c959ed685379812fbce6ba0f5e68f5b22f
Olugbenga Opemipo <rosabelolugbenga@yahoo.com>
2020-01-19 22:25:58 +0100

    trying to add  encapsulation and abstraction

---
+++ StackAndHeap/Car.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndHeap
+{
+    public  class Car //the public helps in encapsulation
+    {
+        public string  color    { get; set; }
+        public string  type { get; set; }
+        public static void Move()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
