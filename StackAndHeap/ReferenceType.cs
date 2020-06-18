90ecbc8fb9d91e87acdc9b0d1bc91588f08f080f
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-18 23:40:58 +0100

    example of reference type

---
+++ StackAndHeap/ReferenceType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndHeap
+{
+    class ReferenceType
+    {
+
+        //example of reference type
+        //class
+      //  interface
+        //delegate
+        //object
+        //string
+    }
+}
