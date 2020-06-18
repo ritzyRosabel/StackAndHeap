659b6f2cf9592afd5ec617ffa3f3b597a5e2ff22
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-18 23:38:55 +0100

    examples of value type

---
+++ StackAndHeap/ValueType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndHeap
+{
+    class ValueType
+    {
+        //example of value type
+        //bool
+       // byte
+        //char
+        //decimal
+        //double
+        //enum
+        //float
+        //int
+        //long
+        //sbyte
+        //short
+        //struct
+        //uint
+        //ulong
+        //ushort
+    }
+}
