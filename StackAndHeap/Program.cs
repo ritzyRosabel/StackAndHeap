b8475246e7bd55361f58cd950c7841636e1c8ba8
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-18 23:32:21 +0100

    notes on stack and heap

--- StackAndHeap/Program.cs
+++ StackAndHeap/Program.cs
@@ -11,6 +11,8 @@ namespace StackAndHeap
    // i need to go over this again i dont understand what i did here
         static void Main(string[] args)
         {
+            //Think of the Stack as a series of boxes stacked one on top of the next.  We keep track of what's going on in our application by stacking another box on top every time we call a method (called a Frame).  We can only use what's in the top box on the stack.  When we're done with the top box (the method is done executing) we throw it away and proceed to use the stuff in the previous box on the top of the stack. The Heap is similar except that its purpose is to hold information (not keep track of execution most of the time) so anything in our Heap can be accessed at any time.
+            //The Stack is self-maintaining, meaning that it basically takes care of its own memory management.  When the top box is no longer used, it's thrown out.  The Heap, on the other hand, has to worry about Garbage collection (GC) - which deals with how to keep the Heap clean 
             BirthYear birthYear = new BirthYear("opemipo",14); //Reference to birthyear stored in the stack although birth year it self is stored in the heap
             birthYear.AddYear();//makes changes my adding a year to age in the heap
             birthYear.DisplayResult();//display the values
